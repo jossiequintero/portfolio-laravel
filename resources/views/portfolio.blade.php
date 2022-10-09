309663a4a8c1afff8377cee2eedde310450c8022
Jossie Quintero <quinterojosy@gmail.com>
2022-10-09 16:29:04 -0500

    Depuracion en laravel

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -6,9 +6,20 @@
 
     <h1>PORTFOLIO</h1>
     <ul>
+        {{--  dd(var)  --}}
+        {{--  var_dump(var)  --}}
+        {{-- dump() --}}
         @isset($proyectos)
             @forelse ($proyectos as $proyecto)
-                <li>{{ $proyecto['title'] }}</li>
+                <li>{{ $proyecto['title'] }}
+                    <pre><?php dump($loop); ?></pre>
+                    @if ($loop->first)
+                        <small> Soy el primero</small>
+                    @elseif($loop->last)
+                        <small> Soy el ultimo</small>
+                    @endif
+                    <small> Soy el numero {{ $loop->iteration }}</small>
+                </li>
             @empty
                 <li>Nenhum proyecto cadastrado</li>
             @endforelse
@@ -16,5 +27,6 @@
             Ocurrio un error
         @endisset
     </ul>
+    {{ print_r($proyectos) }}
 
 @endsection()
